51fe48671accdead1ef83ce1bb6e319a47b3d2c1
Ian_S <85195921+IanSalomao@users.noreply.github.com>
2024-05-21 21:27:58 -0300

    Joining the two sections
    Combining the initial sections and the interactive section with flash cards.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
-function splitDrawing(drawingType) {
+function splitDrawing(drawingType, numVariable) {
   let tableImage = document.querySelector("#card-" + drawingType + ">.container>.table");
   let drawingImage = document.querySelector("#card-" + drawingType + ">.container>.drawing");
 
-  tableImage.style.backgroundImage = "url('assets/TABLE-AND_2.png')";
-  drawingImage.style.backgroundImage = "url('assets/AND_2.png')";
+  let numDrawing = parseInt(document.querySelector("#card-" + drawingType + ">.container>.drawing").style.backgroundImage.match(/\d+/)[0]);
+
+  if (numDrawing == numVariable) {
+    tableImage.style.backgroundImage = "url('assets/TABLE-" + drawingType.toUpperCase() + "_1.png')";
+    drawingImage.style.backgroundImage = "url('assets/" + drawingType.toUpperCase() + "_1.png')";
+  } else {
+    tableImage.style.backgroundImage = "url('assets/TABLE-" + drawingType.toUpperCase() + "_" + (numDrawing + 1) + ".png')";
+    drawingImage.style.backgroundImage = "url('assets/" + drawingType.toUpperCase() + "_" + (numDrawing + 1) + ".png')";
+  }
 }
